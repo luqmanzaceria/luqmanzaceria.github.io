d7b0e4b581f46faf6bb181ee7e62e950fd8454a2
Luqman Zaceria <luqmanzaceria@gmail.com>
2023-12-17 20:17:47 -0800

    Add source branch

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,27 @@
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
+
+require(`dotenv`).config({
+  path: `.env`,
+})
+
 module.exports = {
   siteMetadata: {
     title: `Personal Website`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: ["gatsby-plugin-google-gtag"]
+  plugins: [
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // your google analytics tracking id
+        trackingIds: [process.env.GOOGLE_ANALYTICS_ID],
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // enable ip anonymization
+        anonymize: true,
+      },
+    }
+  ]
 };
