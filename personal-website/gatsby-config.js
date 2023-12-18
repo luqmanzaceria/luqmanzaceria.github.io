5b87125ebf9993819ba8c1ef4b09b7c9bf7c403b
Luqman Zaceria <luqmanzaceria@gmail.com>
2023-12-17 20:00:55 -0800

    Make Gatsby development server start

--- personal-website/gatsby-config.js
+++ personal-website/gatsby-config.js
@@ -1,9 +1,27 @@
 /**
  * @type {import('gatsby').GatsbyConfig}
  */
+
+require(`dotenv`).config({
+  path: `.env`,
+})
+
 module.exports = {
   siteMetadata: {
-    siteUrl: `https://www.yourdomain.tld`,
+    title: `Personal Website`,
+    siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: [],
-}
+  plugins: [
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // your google analytics tracking id
+        trackingIds: [process.env.GOOGLE_ANALYTICS_ID],
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // enable ip anonymization
+        anonymize: true,
+      },
+    }
+  ]
+};
