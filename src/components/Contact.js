7701999134be549f47659c854e3407854fd27543
Luqman Zaceria <luqmanzaceria@gmail.com>
2023-12-19 19:00:54 -0800

    Add contact section

---
+++ src/components/Contact.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import linkedin from '../images/linkedin.png'
+import github from '../images/github.png'
+import email from '../images/email.png'
+
+const Contact = () => {
+    return (
+        <>
+            <meta charset="UTF-8"></meta>
+            <div id="contact" class='flex flex-col pt-24'>
+                <h2 class='text-3xl font-light px-8 sm:px-16 md:px-32 pb-8'>Contact</h2>
+                <a class='px-14 sm:px-28 md:px-40 pt-4 text-lg hover:underline hover:text-blue-500' href='mailto:luqmanzaceria@gmail.com'>luqmanzaceria@gmail.com</a>
+                <div class="flex justify-center space-x-4 py-16">
+                    <a href="mailto:luqmanzaceria@gmail.com" target="_blank" rel="noopener noreferrer" class="block">
+                        <img src={email} alt="Email" class="h-16 w-16" />
+                    </a>
+                    <a href="https://www.linkedin.com/in/luqmanzaceria" target="_blank" rel="noopener noreferrer" class="block">
+                        <img src={linkedin} alt="LinkedIn" class="h-16 w-16" />
+                    </a>
+                    <a href="https://github.com/luqmanzaceria" target="_blank" rel="noopener noreferrer" class="block">
+                        <img src={github} alt="GitHub" class="h-16 w-16" />
+                    </a>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Contact;
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,6 +8,7 @@ import Education from '../components/Education'
 import Experience from '../components/Experience'
 import Projects from '../components/Projects'
 import Skills from '../components/Skills'
+import Contact from '../components/Contact'
 
 export default function Home() {
   return (
@@ -19,6 +20,7 @@ export default function Home() {
       <Experience/>
       <Projects/>
       <Skills/>
+      <Contact/>
     </>
   );
 }
